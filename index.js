23494084d097ee4bc817a2ca70dcf1fb59e1bb78
Raj Khajanchi <rajvkhajanchi@gmail.com>
2026-03-18 12:24:29 +0530

    Minor fixes in the code

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 module.exports = {
   rules: {
-    'no-quadratic-loops': require('./lib/rules/no-quadratic-loops'),
-    'no-sync-in-loop': require('./lib/rules/no-sync-in-loop'),
-    'no-large-json-parse-in-loop': require('./lib/rules/no-large-json-parse-in-loop'),
-    'react-no-inline-object-creation': require('./lib/rules/react-no-inline-object-creation'),
-    'no-unnecessary-array-clone': require('./lib/rules/no-unnecessary-array-clone')
+    'no-quadratic-loops': require('./src/lib/rules/no-quadratic-loops'),
+    'no-sync-in-loop': require('./src/lib/rules/no-sync-in-loop'),
+    'no-large-json-parse-in-loop': require('./src/lib/rules/no-large-json-parse-in-loop'),
+    'react-no-inline-object-creation': require('./src/lib/rules/react-no-inline-object-creation'),
+    'no-unnecessary-array-clone': require('./src/lib/rules/no-unnecessary-array-clone')
   },
   configs: {
     recommended: {
